5ad4fb1df1af2f17e286dea3a4fa78b3768ed61e
roman-boginich <roman.boginich@sharp-dev.net>
2023-10-13 17:35:59 +0300

    Lesson 21-27

---
+++ src/Services/Catalog/Catalog.API/Controllers/CatalogController.cs
@@ -0,0 +1,90 @@
+﻿    using System.Net;
+using Catalog.API.Entities;
+using Catalog.API.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Catalog.API.Controllers;
+
+[ApiController]
+[Route("api/v1/[controller]")]
+public class CatalogController : ControllerBase
+{
+    private readonly ILogger<CatalogController> _logger;
+    private readonly IProductRepository _productRepository;
+
+    public CatalogController(ILogger<CatalogController> logger, IProductRepository productRepository)
+    {
+        _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        _productRepository = productRepository ?? throw new ArgumentNullException(nameof(productRepository));
+    }
+
+    [HttpGet]
+    [ProducesResponseType(typeof(IEnumerable<Product>), (int) HttpStatusCode.OK)]
+    public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
+    {
+        var products = await _productRepository.GetProducts();
+        return Ok(products);
+    }
+
+    [HttpGet("{id:length(24)}", Name = "GetProduct")]
+    [ProducesResponseType((int) HttpStatusCode.NotFound)]
+    [ProducesResponseType(typeof(Product), (int) HttpStatusCode.OK)]
+    public async Task<ActionResult<Product>> GetProductById(string id)
+    {
+        var product = await _productRepository.GetProduct(id);
+        if (product is null)
+        {
+            _logger.LogError($"{nameof(Product)} with id: {id}, not found.");
+            return NotFound();
+        }
+
+        return Ok(product);
+    }
+
+    [HttpGet]
+    [Route("[action]/{category}", Name = "GetProductByCategory")]
+    [ProducesResponseType(typeof(IEnumerable<Product>), (int) HttpStatusCode.OK)]
+    public async Task<ActionResult<IEnumerable<Product>>> GetProductByCategory(string category)
+    {
+        var products = await _productRepository.GetProductByCategory(category);
+        return Ok(products);
+    }
+
+    [HttpGet]
+    [Route("[action]/{name}", Name = "GetProductByName")]
+    [ProducesResponseType((int) HttpStatusCode.NotFound)]
+    [ProducesResponseType(typeof(IEnumerable<Product>), (int) HttpStatusCode.OK)]
+    public async Task<ActionResult<IEnumerable<Product>>> GetProductByName(string name)
+    {
+        var items = await _productRepository.GetProductByName(name);
+        if (items == null)
+        {
+            _logger.LogError($"Products with name: {name} not found.");
+            return NotFound();
+        }
+
+        return Ok(items);
+    }
+
+    [HttpPost]
+    [ProducesResponseType(typeof(Product), (int) HttpStatusCode.OK)]
+    public async Task<ActionResult<Product>> CreateProduct([FromBody] Product product)
+    {
+        await _productRepository.CreateProduct(product);
+        return CreatedAtRoute("GetProduct", new {id = product.Id}, product);
+    }
+
+    [HttpPut]
+    [ProducesResponseType(typeof(Product), (int) HttpStatusCode.OK)]
+    public async Task<IActionResult> UpdateProduct([FromBody] Product product)
+    {
+        return Ok(await _productRepository.UpdateProduct(product));
+    }
+
+    [HttpDelete("{id:length(24)}", Name = "DeleteProduct")]
+    [ProducesResponseType(typeof(Product), (int) HttpStatusCode.OK)]
+    public async Task<IActionResult> DeleteProductById(string id)
+    {
+        return Ok(await _productRepository.DeleteProduct(id));
+    }
+}
--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,23 +1,63 @@
+using Catalog.API.Data;
+using Catalog.API.Data.Interfaces;
+using Catalog.API.Repositories;
+using Catalog.API.Repositories.Interfaces;
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Microsoft.OpenApi.Models;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddScoped<ICatalogContext, CatalogContext>();
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+
+builder.Services.AddHealthChecks().AddMongoDb(
+    builder.Configuration["DatabaseSettings:ConnectionString"] ?? string.Empty, "MongoDb Health", HealthStatus.Degraded);
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+//builder.Services.AddSwaggerGen(c => { c.SwaggerDoc("v1", new OpenApiInfo {Title = "Catalog.API", Version = "v1"}); });
 
-var app = builder.Build();
+
+var app = builder.Build(); 
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.UseDeveloperExceptionPage();
     app.UseSwagger();
     app.UseSwaggerUI();
+    //app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Catalog.API v1"));
 }
 
+
+app.UseRouting();
+ 
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.MapHealthChecks("/hc", new HealthCheckOptions
+{
+    Predicate = _ => true,
+    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+});
+
+//app.UseEndpoints(endpoints =>
+//{
+//    endpoints.MapControllers();                
+//    endpoints.MapHealthChecks("/hc", new HealthCheckOptions
+//    {
+//        Predicate = _ => true,
+//        ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+//    });
+//});
+
 
 app.Run();
